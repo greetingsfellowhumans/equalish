f5a335a960a2f99a6fdd9611ff3d3918756302f6
greetingsfellowhumans <accounts@aaronjprice.com>
2026-09-03 21:34:46 -0700

    version: update to v1.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Equalish.MixProject do
   def project do
     [
       app: :equalish,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       docs: docs(),
